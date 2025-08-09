7a451306746c1a207bb9a43755f882172ac561e6
NabihaMahboob <nab.mahboob@gmail.com>
2025-08-09 14:06:19 -0400

    database connection

--- database/db_connection.php
+++ database/db_connection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $database = "users";
 
-$conn = new mysqli($servername, $username, $password, $database, null, '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock');
+$conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
